e68176952821d0cc39e41bb5e50ad51b2c85dc01
StefanPowell <stefanpowell100@icloud.com>
2020-08-01 02:31:21 -0400

    added sql connection, SQLException : no suitable driver

--- Java_Doctor/healthi/src/main/java/healthi/Main.java
+++ Java_Doctor/healthi/src/main/java/healthi/Main.java
@@ -9,17 +9,39 @@ import java.awt.FlowLayout;
 import javax.swing.*;  
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
+
 
 /**
  *
  * @author Stefan Powell : powellstefan100@gmail.com
  */
 public class Main {
-
+    
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver"; 
     static JTextField firstname, lastname, email;
             
     public static void main(String[] args) {
+        String ip = "104.154.57.18";
+        String username = "spowell375";
+        String password = "mari$a1998";
+        //connect(ip,username, password);
         display_startscreen();
+    }
+    
+    public static void connect(String ip, String user, String password) {
+            Connection conn1 = null;
+            Connection conn2 = null;
+            Connection conn3 = null;
+            try {
+                String url2 = "jdbc:mysql://104.154.57.18:3306/mysql?user=spowell375&password=mari$a1998";
+                conn2 = DriverManager.getConnection(url2);
+                if (conn2 != null) {
+                    System.out.println("Connected to the database test2");
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+            }
     }
     
     public static void signupscreen(){
