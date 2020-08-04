9a0f19204f0c7410fe83a93f995ded548aad1273
StefanPowell <stefanpowell100@icloud.com>
2020-08-04 01:24:05 -0400

    start point for Pharmacy, added Junit testing

---
+++ Java_Pharmacy/Healthi2/src/test/java/com/mycompany/healthi2/newpackage/MainTest.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.healthi2.newpackage;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author stefan
+ */
+public class MainTest {
+    
+    public MainTest() {
+    }
+
+    /**
+     * Test of add method, of class Main.
+     */
+    @org.junit.jupiter.api.Test
+    public void testAdd() {
+        int z = (3+2);
+        int expected = 5;
+        assertEquals(expected, z);
+    }
+    
+}
